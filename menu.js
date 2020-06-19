ef9ac9f3ac8810f037675d2078848ceea974d945
ngaretou <corey.garrett@sim.org>
2020-06-19 09:45:36 +0000

    Multilingual menus

--- menu.js
+++ menu.js
@@ -1,156 +1,207 @@
 //This is js for the main process, called from main.js. Renderer process js is in ./HTML/script.js
-//Here you have main menu, context menu, and opening call for the copyright window. 
-const electron = require('electron')
-const { app, Menu, shell, BrowserWindow } = electron
-//get our localisation from mydata
-const myData = require('./mydata')
-//This is the array with information about our collections in an array
-const initialState = myData.collections
-const transl = myData.otherText[0]
-let MyAppVersion = app.getVersion()
+//Here you have main menu, context menu, and opening call for the copyright window.
+const {
+  electron,
+  remote,
+  app,
+  Menu,
+  shell,
+  BrowserWindow,
+  window,
+  ipcMain,
+  ipcRenderer,
+} = require("electron");
+//get our localisation (changeable strings depending on language)
+//and otherText strings (things that don't change based on language) from mydata
+const myData = require("./mydata");
+const appText = myData.otherText;
+const transl = myData.myTranslations;
 
-let copyrightWindow
-let mainMenu
-let contextMenu
-let mainWindow
+let MyAppVersion = app.getVersion();
 
-
-//Copyright window
-
-//Copyright & license window to show from the above menu
-// var copyrightWindow = null
-
-function openAboutWindow() {
-	if (copyrightWindow) {
-	  copyrightWindow.focus()
-	  return
-	}
-	
-	copyrightWindow = new BrowserWindow({
-	  parent: mainWindow, 
-	  modal: true, 
-	  width: 600,
-	  height: 650,
-	  title: 'Copyright // Version ' + MyAppVersion,
-	  minimizable: false,
-	  fullscreenable: false,
-	  resizable: false,
-	  alwaysOnTop: true,
-	  menu: null,
-	  show: false,
-	  webPreferences: {nodeIntegration: true, enableRemoteModule: true} 
-	})
-  
-  
-	copyrightWindow.loadFile('HTML/copy.html')
-	
-	  copyrightWindow.once('ready-to-show', () => {
-		copyrightWindow.show()
-		//copyrightWindow.webContents.openDevTools()
-	  })
-  
-	  copyrightWindow.once('blur', () => {copyrightWindow.close()})
-  
-	copyrightWindow.on('closed', function() {
-	  copyrightWindow = null
-	})
-  }
-  //End of copyright window
-
-// Here we have separate menus for Mac (darwin) vs Win&Linux. To gain some consistency across the operating systems, we show the menu only in Mac.
-// If Win or Linux, it returns null below, which makes the menu empty, and thus hidden, and all functions are available from context menu. 
-// Because of that our menu and contextmenu are basically identical, except that the mainMenu needs an extra level on top, with the main menu elements
-// as a submenu underneath that. So set up the core menu, then either dress it with that top Menu level or not and return the objects. 
-
-let coreMenuSection1 = [
-	{
-		label: transl.menuZoomIn,
-		role: "zoomIn"
-	},
-	{
-		label: transl.menuZoomOut, 
-		role: "zoomOut"
-	},
-	{
-		label: transl.menuResetZoom, 
-		role: "resetZoom"
-	},
-	{
-		type: "separator"
-	},
-	{
-		label: transl.menuCopy,
-		accelerator: "CmdOrCtrl+C",
-		selector: "copy:"
-	},
-	{
-		label: transl.menuSelectAll,
-		role: "selectAll"
-	},
-	{
-		type: "separator"
-	},
-	{
-		label: transl.menutoggleDevTools, 
-		role: "toggleDevTools"
-	},
-	{
-	label: transl.menuOpenAboutWin, 
-		click() {openAboutWindow()}
-	}
-]
-
-//Now we have the first part of the menu as the array coreMenuDefinition
-//If we have a website prompt and a URL, make it the next part of the menu, 
-//but if we don't have either one, then skip on to the final section of the menu
-if (!(transl.menuWebsite === '') && !(transl.menuWebURL === '')) {coreMenuSection2 = [
-	{
-		type: "separator"
-	},
-	{
-	label: transl.menuWebsite,
-	click() {shell.openExternal(transl.menuWebURL);}
-	},
-	{
-		type: "separator"
-	}
-	]} else {coreMenuSection2 = [
-	{
-		type: "separator"
-	}]
+let copyrightWindow;
+let mainMenu;
+let contextMenu;
+let mainWindow;
+function goHigh() {
+  console.log("in gohigh");
 }
 
+//Copyright & license window to show from the menu
 
-//This is the last section of the menu 
-coreMenuSection3 = [
-	{
-		label: transl.menuQuit,
-		role: "quit"
-	},
-	{label: transl.thisAppName + " " + MyAppVersion, 
-		enabled: false
-	}
-]
+function openAboutWindow() {
+  if (copyrightWindow) {
+    copyrightWindow.focus();
+    return;
+  }
+
+  copyrightWindow = new BrowserWindow({
+    parent: mainWindow,
+    modal: true,
+    width: 600,
+    height: 650,
+    title: "Copyright // Version " + MyAppVersion,
+    minimizable: false,
+    fullscreenable: false,
+    resizable: false,
+    alwaysOnTop: true,
+    menu: null,
+    show: false,
+    webPreferences: { nodeIntegration: true, enableRemoteModule: true },
+  });
+
+  copyrightWindow.loadFile("HTML/copy.html");
+
+  copyrightWindow.once("ready-to-show", () => {
+    copyrightWindow.show();
+    //copyrightWindow.webContents.openDevTools()
+  });
+
+  copyrightWindow.once("blur", () => {
+    copyrightWindow.close();
+  });
+
+  copyrightWindow.on("closed", function () {
+    copyrightWindow = null;
+  });
+}
+//End of copyright window
+
+//Before going about constructing the menu, get the displayLang
+console.log("global.globalDisplayLang is set to " + global.globalDisplayLang);
+
+displayLang = global.globalDisplayLang;
+console.log(displayLang);
+console.log(Object.keys(transl.langName));
+
+// Here we have separate menus for Mac (darwin) vs Win&Linux. To gain some consistency across the operating systems, we show the menu only in Mac.
+// If Win or Linux, it returns null below, which makes the menu empty, and thus hidden, and all functions are available from context menu.
+// Because of that our menu and contextmenu are basically identical, except that the mainMenu needs an extra level on top, with the main menu elements
+// as a submenu underneath that. So set up the core menu, then either dress it with that top Menu level or not and return the objects.
+
+let coreMenuSection1 = [
+  {
+    label: transl.menuZoomIn[displayLang],
+    role: "zoomIn",
+  },
+  {
+    label: transl.menuZoomOut[displayLang],
+    role: "zoomOut",
+  },
+  {
+    label: transl.menuResetZoom[displayLang],
+    role: "resetZoom",
+  },
+  {
+    type: "separator",
+  },
+  {
+    label: transl.menuCopy[displayLang],
+    accelerator: "CmdOrCtrl+C",
+    selector: "copy:",
+  },
+  {
+    label: transl.menuSelectAll[displayLang],
+    role: "selectAll",
+  },
+  {
+    type: "separator",
+  },
+  {
+    label: transl.menutoggleDevTools[displayLang],
+    role: "toggleDevTools",
+  },
+  {
+    label: transl.menuOpenAboutWin[displayLang],
+    click() {
+      openAboutWindow();
+    },
+  },
+];
+
+//Language switcher
+
+if (!(Object.entries(transl.langName).length === 1)) {
+  var langMenuDefinition = [];
+  console.log("object keys length for lang name is not equal to 1");
+  for (let [key, value] of Object.entries(transl.langName)) {
+    console.log(`${key}: ${value}`);
+    miniLangMenuDefinition = {
+      label: `${value}`,
+      click() {
+        //langChange(`${key}`);
+        ipcRenderer.send("change-lang", `${key}`);
+      },
+    };
+    langMenuDefinition.push(miniLangMenuDefinition);
+  }
+  console.log(langMenuDefinition);
+
+  coreMenuSection2 = [
+    {
+      label: transl.menuLangSwitch[displayLang],
+      submenu: langMenuDefinition,
+    },
+  ];
+} //If the length is not equal to 1 that means there are multiple languages, so put in lang switcher
+//If there's only one display language it keeps rolling, skipping the language switcher
+
+//Now we have the first part of the menu as the array coreMenuDefinition
+//If we have a website prompt and a URL, make it the next part of the menu,
+//but if we don't have either one, then skip on to the final section of the menu
+if (!(transl.menuWebsite[displayLang] === "") && !(appText.menuWebURL === "")) {
+  coreMenuSection3 = [
+    {
+      type: "separator",
+    },
+    {
+      label: transl.menuWebsite[displayLang],
+      click() {
+        shell.openExternal(appText.menuWebURL);
+      },
+    },
+    {
+      type: "separator",
+    },
+  ];
+} else {
+  coreMenuSection3 = [
+    {
+      type: "separator",
+    },
+  ];
+}
+
+//This is the last section of the menu
+coreMenuSection4 = [
+  {
+    label: transl.menuQuit[displayLang],
+    role: "quit",
+  },
+  { label: appText.thisAppName + " " + MyAppVersion, enabled: false },
+];
 //This operator takes multiple arrays (coreMenuSections) and joins them into one array, coreMenuDefinition
 //https://dmitripavlutin.com/operations-on-arrays-javascript/#42-spread-operator
-coreMenuDefinition = [...coreMenuSection1, ...coreMenuSection2, ...coreMenuSection3];
+coreMenuDefinition = [
+  ...coreMenuSection1,
+  ...coreMenuSection2,
+  ...coreMenuSection3,
+  ...coreMenuSection4,
+];
 
 //Now take that core definition array that we've assembled and include it in the Menu array wrapper
-//but only enable it if we're on macOS (darwin) 
-if (process.platform === 'darwin') {
-	mainMenu = Menu.buildFromTemplate([
-		{label: 'Menu',
-		submenu: coreMenuDefinition
-		}
-	])
+//but only enable it if we're on macOS (darwin)
+if (process.platform === "darwin") {
+  mainMenu = Menu.buildFromTemplate([
+    { label: "Menu", submenu: coreMenuDefinition },
+  ]);
 } else {
-	mainMenu = null
-};
+  mainMenu = null;
+}
 
 //The context menu just takes that core menu
-contextMenu = Menu.buildFromTemplate(coreMenuDefinition)
-	
+contextMenu = Menu.buildFromTemplate(coreMenuDefinition);
 
 //export the relevant elements so that they will be available when the require hits in main.js
-module.exports.mainMenu = mainMenu
-module.exports.contextMenu = contextMenu
+module.exports.mainMenu = mainMenu;
+module.exports.contextMenu = contextMenu;
--- mydata.js
+++ mydata.js
@@ -1,93 +1,148 @@
 //To customize this app, you should be able to add all the information you need here.
 
-const collections = [];
-const otherText = [];
-
 //For each HTML collection, add the following. Delete any extra entries or copy and paste to add.
-collection = {
-  name: "Kàddug Yàlla", //the name of the collection
-  icon: "bookmark", //the Material icon to use. Go to https://fontdrop.info/ and drop the MaterialIcons font in the HTML folder.
-  //Check which icon you want to use, and note the unicode character. You can directly use the unicode
-  //character like this: icon: "&#xE031;", or you can look up the name of the icon in the MaterialIcons.txt
-  //file in the HTML folder and grab the name there.
-  folder: "org.mbs.kyg.wol", //The folder in the HTML folder in which this collection is found.
-  fileToView: "index.html", //The first file to view on a new install - usually index.html but not necessarily.
-  cssClass: "icon-text", //For Roman script (abc) leave as icon-text. For Arabic script icon-text-arabic.
-  horizontalLineFollows: false, //If you want a horizontal line following this entry, put true.
+var collections = [
+  {
+    name: "Kàddug Yàlla", //the name of the collection
+    icon: "bookmark", //the Material icon to use. Go to https://fontdrop.info/ and drop the MaterialIcons font in the HTML folder.
+    //Check which icon you want to use, and note the unicode character. You can directly use the unicode
+    //character like this: icon: "&#xE031;", or you can look up the name of the icon in the MaterialIcons.txt
+    //file in the HTML folder and grab the name there.
+    folder: "org.mbs.kyg.wol", //The folder in the HTML folder in which this collection is found.
+    fileToView: "index.html", //The first file to view on a new install - usually index.html but not necessarily.
+    cssClass: "icon-text", //For Roman script (abc) leave as icon-text. For Arabic script icon-text-arabic.
+    horizontalLineFollows: false, //If you want a horizontal line following this entry, put true.
+  },
+  {
+    name: "Injiil 2020",
+    icon: "add_circle",
+    folder: "org.mbs.kyg.wol.2020",
+    fileToView: "index.html",
+    cssClass: "icon-text",
+    horizontalLineFollows: false,
+  },
+  {
+    name: "ڪَدُّگْ يَلَّ گِ",
+    icon: "polymer",
+    folder: "org.mbs.kygma.wol",
+    fileToView: "index.html",
+    cssClass: "icon-text-arabic",
+    horizontalLineFollows: true,
+  },
+  {
+    name: "Ab Jukki",
+    icon: "format_align_left",
+    folder: "org.mbs.chrono.wol.alfa",
+    fileToView: "index.html",
+    cssClass: "icon-text",
+    horizontalLineFollows: false,
+  },
+  {
+    name: "اَبْ جُڪِّ",
+    icon: "format_align_right",
+    folder: "org.mbs.chrono.wol.ajami",
+    fileToView: "index.html",
+    cssClass: "icon-text-arabic",
+    horizontalLineFollows: false,
+  },
+];
+
+var myTranslations = {
+  langName: {
+    //The invitation to open and close the second window, e.g. "One Pane" "Two Pane":
+    en: "English",
+    fr: "Français",
+    wo: "Wolof",
+  },
+  menuLangSwitch: {
+    en: "Language",
+    fr: "Langue d'interface",
+    wo: "Làmmiñu diisookaay bi",
+  },
+  invToOpen: {
+    //The invitation to open and close the second window, e.g. "One Pane" "Two Pane":
+    en: "Two Panes",
+    fr: "Deux Volets",
+    wo: "Ñaari laf",
+  },
+  invToClose: {
+    en: "One Pane",
+    fr: "Un Volet",
+    wo: "Wenn laf",
+  },
+  giveFeedback: {
+    //If this is empty, then there will be no feedback button.
+    //Make sure it looks like this if you want the button:
+    //ln: 'Feedback',
+    //or like this if you want no feedback button:
+    //ln: '',
+    en: "Feedback",
+    fr: "Feedback",
+    wo: "Feedback",
+  },
+  giveFeedbacksubject: {
+    //the subject line to include automatically in any feedback email.
+    en: "Feedback on KYG desktop app",
+    fr: "Feedback sur KYG appli pour ordinateur",
+    wo: "Feedback sur KYG appli pour ordinateur",
+  },
+  menuZoomIn: {
+    en: "Zoom +",
+    fr: "Zoom +",
+    wo: "Zoom +",
+  },
+  menuZoomOut: {
+    en: "Zoom -",
+    fr: "Zoom -",
+    wo: "Zoom -",
+  },
+  menuResetZoom: {
+    en: "Zoom 100%",
+    fr: "Zoom 100%",
+    wo: "Zoom 100%",
+  },
+  menuCopy: {
+    en: "Copy",
+    fr: "Copier",
+    wo: "Copier",
+  },
+  menuSelectAll: {
+    en: "Select All",
+    fr: "Sélectionner tout",
+    wo: "Sélectionner tout",
+  },
+  menutoggleDevTools: {
+    en: "Open DevTools",
+    fr: "Outils de développement",
+    wo: "Outils de développement",
+  },
+  menuWebsite: {
+    //The text prompt on the option to open the website. Note the URL is below in otherText.
+    en: "More on the web",
+    fr: "Notre site web",
+    wo: "Xoolal sunu ëttu internet",
+  },
+
+  menuOpenAboutWin: {
+    en: "Copyright && license",
+    fr: "Copyright && license",
+    wo: "Copyright && license",
+  },
+
+  menuQuit: {
+    en: "Quit",
+    fr: "Quitter",
+    wo: "Quitter",
+  },
 };
-collections.push(collection);
 
-collection = {
-  name: "Injiil 2020",
-  icon: "add_circle",
-  folder: "org.mbs.kyg.wol.2020",
-  fileToView: "index.html",
-  cssClass: "icon-text",
-  horizontalLineFollows: false,
-};
-collections.push(collection);
-
-collection = {
-  name: "ڪَدُّگْ يَلَّ گِ",
-  icon: "polymer",
-  folder: "org.mbs.kygma.wol",
-  fileToView: "index.html",
-  cssClass: "icon-text-arabic",
-  horizontalLineFollows: true,
-};
-collections.push(collection);
-
-collection = {
-  name: "Ab Jukki",
-  icon: "format_align_left",
-  folder: "org.mbs.chrono.wol.alfa",
-  fileToView: "index.html",
-  cssClass: "icon-text",
-  horizontalLineFollows: false,
-};
-collections.push(collection);
-
-collection = {
-  name: "اَبْ جُڪِّ",
-  icon: "format_align_right",
-  folder: "org.mbs.chrono.wol.ajami",
-  fileToView: "index.html",
-  cssClass: "icon-text-arabic",
-  horizontalLineFollows: false,
-};
-collections.push(collection);
-
-myTranslations = {
-  //The invitation to open and close the second window, e.g. "One Pane" "Two Pane":
-  transLang: "French",
-  invToOpen: "Ñaari laf",
-  invToClose: "Wenn laf",
-
-  //If this is null, then there will be no feedback button. Make sure it looks like this if you want the button:
-  //giveFeedback: 'Feedback',
-  //or like this if you want no feedback button:
-  //giveFeedback: '',
-  giveFeedback: "Feedback",
-  giveFeedbackemail: "equipedevmbs@gmail.com", //the email address to send feedback to
-  giveFeedbacksubject: "Feedback on KYG desktop app", //the subject line to include automatically in any feedback email.
-
+var otherText = {
   thisAppName: "Kàddug Yàlla gi", //The name that you want to use in the app title bar etc.
-
-  //Menu items below are for both Mac menu and Mac Win and Lin context menus
-  menuZoomIn: "Zoom +",
-  menuZoomOut: "Zoom -",
-  menuResetZoom: "Zoom 100%",
-  menuCopy: "Copier",
-  menuSelectAll: "Sélectionner tout",
-  menutoggleDevTools: "Outils de développement",
-  menuWebsite: "Notre site web",
-  menuWebURL: "https://sng.al/app",
-  menuOpenAboutWin: "Copyright && license",
-  menuQuit: "Quitter",
+  menuWebURL: "https://sng.al/app", //The URL for 'visit our website'. Leave empty if you have no website.
+  giveFeedbackemail: "equipedevmbs@gmail.com", //the email address to which a user can send feedback
+  defaultLang: "en", //The language code that the app should be initially opened in
 };
-//(This could be just one object, but leaving as an array for now
-//in preparation for multi-language menu support in the future)
-otherText.push(myTranslations);
 
-module.exports.otherText = otherText;
 module.exports.collections = collections;
+module.exports.myTranslations = myTranslations;
+module.exports.otherText = otherText;
